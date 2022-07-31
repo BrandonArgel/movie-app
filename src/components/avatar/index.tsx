c6aebbc29eb112373a1461ffb766b04555a07a6c
BrandonArgel <brandargel@gmail.com>
2022-07-31 03:29:03 -0500

    :sparkles: Default user image

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,3 +1,4 @@
+import AvatarImage from "assets/images/avatar.svg";
 import styles from "./avatar.module.scss";
 const BASE_GRAVATAR_URL = "https://secure.gravatar.com/avatar/";
 interface AvatarProps {
@@ -8,7 +9,7 @@ interface AvatarProps {
 const Avatar = ({ src, alt }: AvatarProps) => {
 	return (
 		<img
-			src={src ? `${BASE_GRAVATAR_URL}${src}` : BASE_GRAVATAR_URL}
+			src={src ? `${BASE_GRAVATAR_URL}${src}` : AvatarImage}
 			alt={alt}
 			className={styles.avatar}
 		/>
